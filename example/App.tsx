62275f8f067d4aaf0ba017c43c3b5a795985d857
Heesang Whang <hwhang0917@gmail.com>
2024-04-21 20:33:18 +0900

    :memo: Update sample images

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,32 @@
+import React from "react";
 import { Watermark } from "../";
 
 function App() {
   return (
-    <main>
-      <Watermark isVisible />
-      <h1>First Section</h1>
-    </main>
+    <React.Fragment>
+      <Watermark isVisible opacity={0.05} color="red" text="sample" />
+      <main
+        style={{
+          display: "grid",
+          width: "100vw",
+          height: "100vh",
+          placeItems: "center",
+        }}
+      >
+        <section>
+          <h1>Login</h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <input type="submit" />
+          </form>
+        </section>
+      </main>
+    </React.Fragment>
   );
 }
 
